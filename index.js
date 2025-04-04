ab41c9473733c677e177deb850cefd260d07f0e8
Guilherme <guilhermetr2005@gmail.com>
2025-04-04 12:27:37 -0300

    Codigo atualizado e pronto

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ let heroi = {
         switch(heroi.tipo){
             case "mago":
                 console.log("o " + heroi.nome + " atacou usando magia")
+                break
             
             case "guerreiro":
                 console.log("o " + heroi.nome + " atacou usando espada")
+                break
 
             case "monge":
                 console.log("o " + heroi.nome + " atacou usando artes marciais")
+                break
 
             case "ninja":
                 console.log("o " + heroi.nome + " atacou usando shuriken")
+                break
+
+            default:
+                console.log("o " + heroi.nome + " ainda nao tem tipo")
+                break
         }
     } 
 }
+
+heroi.atacar()
